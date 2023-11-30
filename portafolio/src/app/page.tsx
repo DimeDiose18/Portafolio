1f9eb32c3de3b8db2e45b8235ae48d94cccdb5af
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-11-30 16:03:31 -0500

    feat: :sparkles: Component Titles ready

--- portafolio/src/app/page.tsx
+++ portafolio/src/app/page.tsx
@@ -1,6 +1,11 @@
+import { Title } from './ui/components/Title';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between mobile:p-10 p-14'>
+      <section>
+      <Title />
+      </section>
       
     </main>
   )
---
+++ portafolio/src/app/ui/components/Title.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { meowScript, ibmPlexMono } from '../fonts'
+
+const Title = () => {
+  return (
+    <div>
+        <div>
+        <h2 className={`${meowScript.className} mobile:text-3xl text-rosy outline-4 font-outline`}>Hi, my name is</h2>
+      </div>
+      <div>
+        <h1 className={`${ibmPlexMono.className} mobile:text-5xl text-whity outline-4 font-outline-2 tracking-wide`}>Dioselyn Lemus</h1>
+      </div>
+      <div>
+        <h2 className={`${ibmPlexMono.className} mobile:text-b text-gray m-`}>Full Stack Developer</h2>
+      </div>
+    </div>
+  )
+}
+
+export { Title } 
