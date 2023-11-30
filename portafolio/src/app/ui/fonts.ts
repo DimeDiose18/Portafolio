2281ffc6b77b060aacc849c0d72d8a9616fc8691
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-11-30 16:04:20 -0500

    feat: :sparkles: New file for google fonts

---
+++ portafolio/src/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Meow_Script, IBM_Plex_Mono } from 'next/font/google';
+
+export  const meowScript = Meow_Script({subsets:['latin'], weight: '400'});
+export const ibmPlexMono = IBM_Plex_Mono({subsets:['latin'], weight: '500'});
