c28a7bc1d6608c5ef586c22ff4d42b6cc5828848
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-11-30 16:03:01 -0500

    style: :lipstick: Update styles for titles component

--- portafolio/tailwind.config.ts
+++ portafolio/tailwind.config.ts
@@ -8,8 +8,8 @@ const config: Config = {
   ],
   theme: {
     colors: {
-      'black': '#1E1E1E',
-      'white': '#EEEEEE', 
+      'blacky': '#1E1E1E',
+      'whity': '#EEEEEE', 
       'gray': '#9C9C9C',
       'sky': '#B6EAFF',
       'rosy': '#FD68C4',
@@ -19,6 +19,24 @@ const config: Config = {
       'yellow': '#F5FC72',
       'orange': '#FF9D5A',
       'pink': '#FC5A88'
+    },
+    fontSize: {
+      sm: '0.8rem',
+      md: '0.875rem',
+      b: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.125rem',
+    },
+    screens: {
+      'mobile': '360px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+      'desktop-2': '1440px',
+      'desktop-3': '1920px'
     },
     extend: {
       backgroundImage: {
