315ce0d4fffdcf8b18b50d0751ea221e7699b29c
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-11-30 19:45:26 -0500

    chore: :lipstick: Update fonts

--- portafolio/src/app/ui/fonts.ts
+++ portafolio/src/app/ui/fonts.ts
@@ -1,4 +1,6 @@
-import { Meow_Script, IBM_Plex_Mono } from 'next/font/google';
+import { Meow_Script, IBM_Plex_Mono, PT_Mono, Prompt } from 'next/font/google';
 
 export  const meowScript = Meow_Script({subsets:['latin'], weight: '400'});
 export const ibmPlexMono = IBM_Plex_Mono({subsets:['latin'], weight: '500'});
+export const ptMono = PT_Mono({subsets:['latin'], weight: '400'});
+export const prompt = Prompt({subsets:['latin'], weight: '400'});
