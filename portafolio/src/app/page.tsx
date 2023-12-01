135775a5b0d5d3ed273d07b98497ae8387f6016f
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-11-30 19:51:11 -0500

    feat: :sparkles: Agg new Button component

--- portafolio/src/app/page.tsx
+++ portafolio/src/app/page.tsx
@@ -1,12 +1,19 @@
+import { ButtonContact } from './ui/components/ButtonContact';
+import { SocialNetworks } from './ui/components/SocialNetworks';
 import { Title } from './ui/components/Title';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between mobile:p-10 p-14'>
+    <main className='flex min-h-screen flex-col items-center mobile:p-10 p-14'>
       <section>
       <Title />
+      </section>
+      <section className='flex gap-5 justify-between w-[100%] m-[3.25rem]'>
+        <SocialNetworks />
+        <ButtonContact />
       </section>
       
     </main>
   )
 }
+
