628c65cc01902c5e5a4f78402abbc824111db604
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-11-30 23:44:02 -0500

    feat: :sparkles: Add new Aboutme component

--- portafolio/src/app/page.tsx
+++ portafolio/src/app/page.tsx
@@ -1,6 +1,10 @@
+import { AboutMe } from './ui/components/AboutMe';
 import { ButtonContact } from './ui/components/ButtonContact';
 import { SocialNetworks } from './ui/components/SocialNetworks';
 import { Title } from './ui/components/Title';
+import { ibmPlexMono } from './ui/fonts';
+import { Arrow } from './ui/icons/Arrow';
+import { Circle } from './ui/icons/Circle';
 
 export default function Home() {
   return (
@@ -8,10 +12,23 @@ export default function Home() {
       <section>
       <Title />
       </section>
-      <section className='flex gap-5 justify-between w-[100%] m-[3.25rem]'>
+      <section className='flex gap-5 justify-between mobile:w-[18.75rem] m-[3.25rem]'>
         <SocialNetworks />
         <ButtonContact />
       </section>
+      <section>
+        <AboutMe />
+        <div className='flex justify-center my-7'>
+          <Arrow />
+        </div>
+      </section>
+      <div>
+      <h1 className={`${ibmPlexMono.className} mobile:text-xl text-whity outline-4 font-outline-3 tracking-wide`}>LANGUAGES AND TOOLS</h1>
+      <div className='transform translate-x-[14.9rem] translate-y-[-0.90rem]'>
+        <Circle />
+      </div>
+      
+      </div>
       
     </main>
   )
