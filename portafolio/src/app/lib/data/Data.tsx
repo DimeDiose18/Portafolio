0d855e404eab9334a8b33845777d9f3173b24a43
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-12-04 15:18:26 -0500

    feat: :seedling: Write data for my  skills

---
+++ portafolio/src/app/lib/data/Data.tsx
@@ -0,0 +1,18 @@
+let SkillsData = [
+  { id: 1, name: 'HTML' },
+  { id: 2, name: 'CSS' },
+  { id: 3, name: 'JavaScript' },
+  { id: 4, name: 'TypeScript' },
+  { id: 5, name: 'ReactJS' },
+  { id: 6, name: 'NextJS' },
+  { id: 7, name: 'NodeJS' },
+  { id: 8, name: 'PostgreSQL' },
+  { id: 9, name: 'Redux' },
+  { id: 10, name: 'Zustand' },
+  { id: 11, name: 'Material UI' },
+  { id: 12, name: 'Express' },
+  { id: 13, name: 'Figma' }
+]
+
+
+export { SkillsData }
